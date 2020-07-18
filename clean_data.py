ec4f53c6077977debb24f44f24c8ea549de5d634
Joshua Green <joshua.green@klarna.com>
2020-07-19 00:46:12 +0200

    add script to clean up raw scraped data

---
+++ clean_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import sys
+
+def clean_data(filename):
+    df = pd.read_csv(filename)
+    df = df.drop(columns=['locations.country', 'locations.city',
+                          'broker_agency_id', 'locations.region',
+                          'locations.street', 'locations.district'])
+
+    df = df[df.selling_price < 8000000]
+
+    df.price = df.price.fillna(df.selling_price)
+    df = df.dropna()
+
+    df.sold_at_date              = pd.to_datetime(df.sold_at_date)
+    df.price                     = df.price.astype('int64')
+    df['locations.municipality'] = df['locations.municipality'].astype('category')
+    df['locations.postal_city']  = df['locations.postal_city'].astype('category')
+
+    df['sppm2']      = df.selling_price / df.living_area
+    df['bid_factor'] = df.selling_price / df.price
+
+    return df
+
+if __name__ == '__main__':
+    df = clean_data(sys.argv[1])
+    df.to_csv(sys.argv[2], index=False)
